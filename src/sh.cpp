b497149a896db52a705b6e98cb9c8f2147b4ee2f
chakpongchung <chakpongchun@gmail.com>
2015-07-21 17:42:42 -0400

    refactor

--- src/sh.cpp
+++ src/sh.cpp
@@ -12,11 +12,11 @@ double sh::a(int l_prime,int m_prime, int l1, int m1, int l2, int m2 )
 {
 	//printf("m_prime:   %d  ", m_prime);
 	//printf(",l_prime:  %d\n", l_prime);
-	double temp = (2 * l1 + 1)*(2 * l2 + 1)*(2 * l_prime + 1) / (4.0*M_PI);          
+	double temp = (2 * l1 + 1)*(2 * l2 + 1)*(2 * l_prime + 1) / (4.0*M_PI);
 	//printf("temp is: %f\n", temp);
-	double first = gsl_sf_coupling_3j(2 * l1, 2 * l2, 2 * l_prime, 2 * 0, 2 * 0, 2 * 0);           
+	double first = gsl_sf_coupling_3j(2 * l1, 2 * l2, 2 * l_prime, 2 * 0, 2 * 0, 2 * 0);
 	//printf("first is: %f\n", first);
-	double second = gsl_sf_coupling_3j(2 * l1, 2 * l2, 2 * l_prime, 2 * m1, 2 * m2, -2 * m_prime); 
+	double second = gsl_sf_coupling_3j(2 * l1, 2 * l2, 2 * l_prime, 2 * m1, 2 * m2, -2 * m_prime);
 	//printf("second is: %f\n", second);
 
 	//printf("a is: %.18f \n", pow(-1, m_prime)*pow(temp, 0.5)*first*second);
@@ -45,13 +45,10 @@ void sh::print_a()
 	printf("*******************************************************************\n");
 	printf("cofficients a(n,m+p,j,p,l,m)                                       \n");
 	printf("for spherical-harmonic expansion test program  -cpchung            \n");
-	printf("data saved to data.csv                                             \n");	
+	printf("data saved to data.csv                                             \n");
 	printf("*******************************************************************\n");
 
-
-	
-
-	int  l1 = 8, m1 = -3, l2 = 21, m2 = -1; //  l1 ,l2 >=0, |m1|<=l1, |m2|<=l2;  
+	int  l1 = 8, m1 = -3, l2 = 21, m2 = -1; //  l1 ,l2 >=0, |m1|<=l1, |m2|<=l2;
 	assert(l1 >= 0 && l2 >= 0);
 	assert(abs(m1) <= l1 && abs(m2) <= l2);
 
@@ -85,18 +82,17 @@ void sh::check_diff()
 	printf("*******************************************************************\n");
 	printf("check_diff() between lfs and rhs                                   \n");
 	printf("for spherical-harmonic expansion  -cpchung                         \n");
-	printf("data saved to data.csv                                             \n");	
+	printf("data saved to data.csv                                             \n");
 	printf("*******************************************************************\n");
 
 
-	
 	//  l1 ,l2 >=0, |m1|<=l1, |m2|<=l2;
-	int  l1 = 18, m1 = -3, l2 = 21, m2 = -1;   
+	int  l1 = 18, m1 = -3, l2 = 21, m2 = -1;
 	assert(l1 >= 0 && l2 >= 0);
 	assert(abs(m1) <= l1 && abs(m2) <= l2);
 
 	//theta θ in [0, π], phi φ in [0,2π);
-	double theta = M_PI *.2, phi = M_PI *.4;  
+	double theta = M_PI *.2, phi = M_PI *.4;
 	assert(theta <= M_PI && 0 <= theta);
 	assert(0 <= phi && phi <= 2 * M_PI);
 	cout << "theta is now: " << theta << " phi is now: " << phi << endl;
@@ -122,14 +118,14 @@ void sh::check_diff()
 
 				sum = sum + a(l,m, l1, m1, l2, m2)*spherical_harmonic(l, m, theta, phi);
 
-				datafile << l << "," << m << "," << l1 << "," << m1 << "," << l2 << "," << m2 << "," 
+				datafile << l << "," << m << "," << l1 << "," << m1 << "," << l2 << "," << m2 << ","
 				<< a(l,m, l1, m1, l2, m2 ) << endl;
 			}
 
 		diff = abs(sum - spherical_harmonic(l1, m1, theta, phi)*spherical_harmonic(l2, m2, theta, phi));
 
 		//continuing conditions: if truncation error not met and index l is not over limit
-		if ( !(diff < truncation_error) && num<20)  
+		if ( !(diff < truncation_error) && num<20)
 		num++;
 	}
 
